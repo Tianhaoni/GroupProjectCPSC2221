239469b29b86724b8d1cc0f8efcfd621169d6280
倪天豪 <nitianhao@Tianhaos-MacBook-Pro.local>
2019-11-18 16:42:02 -0800

    first commit

---
+++ page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head align="center">
+
+    </head>
+    <body>
+        <?php
+            $lgoinID = $_POST['username'];
+            $loginPassword = $_POST['password'];
+
+
+            $servername = "localhost";
+            $username = "root"; // your username 
+            $password = "root"; //your password 
+            $database = "group_project";
+            
+            if ($lgoinID =='12345' and $loginPassword =='12345')
+            {
+                echo "Here is your today job.";
+            }
+            else
+            {
+                header('Location: index1.html');
+                echo 'you are not the admin';
+                exit();
+                
+            }
+
+            $conn = new mysqli($servername, $username, $password, $database);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $query = "SELECT * FROM `Aircrew_member_Cabin_crew` WHERE `Group_id` = 1";
+            $result = $conn->query($query);
+            
+            while($row = $result->fetch_assoc()) {
+                echo "<br> Employee Number: ". $row["Employee_number"];
+            }
+        ?>
+    </body>
+</html>
